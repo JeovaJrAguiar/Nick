47f0a7efa297545d547f20b739ac0e18ecef97bc
Lucas Soares <soares0lucas8@gmail.com>
2023-11-10 18:03:44 -0300

    estrutura inicial da api

---
+++ backend/src/main/java/api/nick/NickBotApiApplication.java
@@ -0,0 +1,13 @@
+package api.nick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NickBotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NickBotApiApplication.class, args);
+	}
+
+}
